78b776fb344c9a9df4db7fa67952f9d4458e4409
youdoumei <youdoumei2@gmail.com>
2016-11-05 11:19:01 +0000

    entries controllers

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,26 @@
 class EntriesController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end

8d4043109eb2b6e4af38a3856def5d5ae335bf9d
youdoumei <youdoumei2@gmail.com>
2016-11-05 07:46:06 +0000

    投稿テーブル再度

--- db/migrate/20161105062219_create_entries.rb
+++
@@ -1,8 +0,0 @@
-class CreateEntries < ActiveRecord::Migration
-  def change
-    create_table :entries do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20161105073931_create_entry_2s.rb
+++ db/migrate/20161105073931_create_entry_2s.rb
@@ -1,7 +1,16 @@
-class CreateEntry2s < ActiveRecord::Migration
+class CreateEntries < ActiveRecord::Migration
   def change
-    create_table :entry_2s do |t|
-
+    create_table :entries do |t|
+      t.integer :entrynumber, null: false #投稿ナンバー
+      #t.? :pic写真の添付
+      t.integer :scale_templatenumber, null: false #指標テンプレート
+      t.string :comment, null: false #コメント
+      t.date :entrydate, null: false #投稿日
+      #返信:
+      t.integer :value1, null: false #評価1:苦味
+      t.integer :value2, null: false #評価2：香り
+      t.integer :value3, null: false #評価3：酸味
+      t.integer :value4, null: false #評価4:コク
       t.timestamps null: false
     end
   end

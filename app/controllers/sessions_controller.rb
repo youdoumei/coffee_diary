374a82286d5d63a18b1be1631c9082152fa0ab70
takahiro_u <youdoumei2@gmail.com>
2016-12-13 00:13:08 +0900

    再度コミット2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+  user = User.authenticate(params[:name], params[:password])
+  if user
+    session[:user_id] = user.id
+  else
+    flash.alert = "名前とパスワードが一致しません。"
+  end
+  redirect_to :root
+
+  def destroy
+  session.delete(:user_id)
+  redirect_to :root
+  end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end

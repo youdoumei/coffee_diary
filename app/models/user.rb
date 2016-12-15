e96266d40ed5cd3ae14a003e1a3c0af2e7d3796e
takahiro_u <youdoumei2@gmail.com>
2016-12-16 02:08:40 +0900

    ユ―ザーテーブルの更新とセッションコントローラーの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  validates :password, presence: { on: :create },
+   confirmation: { allow_blank: true }
+  attr_accessor :password, :password_confirmation
+
+  def password=(val)
+    if val.present?
+      self.hashed_password = BCrypt::Password.create(val)
+    end
+    @password = val
+  end
+
+  class << self
+    def authenticate(name, password)
+      user = find_by(name: name)
+      if user && user.hashed_password.present? &&
+        BCrypt::Password.new(user.hashed_password) == password
+        user
+      else
+        nil
+      end
+    end
+  end
 end

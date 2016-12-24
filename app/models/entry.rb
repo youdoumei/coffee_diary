024fd8ecfed8023c0ad1c0d0d141d2a2ea1b2ead
takahiro_u <youdoumei2@gmail.com>
2016-12-24 17:55:07 +0900

    画像テーブルrを作成してマイグレーションするまで

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,3 @@
 class Entry < ActiveRecord::Base
+  has_many :pictures, dependent: :destroy
 end
--- db/migrate/20161224081435_create_pictures.rb
+++ db/migrate/20161224081435_create_pictures.rb
@@ -1,8 +1,13 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
+      t.references :entry, null: false
+      t.binary :data
+      t.string :content_type
 
       t.timestamps null: false
     end
+
+    add_index :entry_pictures, :entry_id
   end
 end

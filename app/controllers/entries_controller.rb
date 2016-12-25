cdaa963c722ad93115c90aa194f0b1caad9bb6f4
takahiro_u <youdoumei2@gmail.com>
2016-12-25 16:33:31 +0900

    Picture再編集

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -29,7 +29,7 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(params[:entry])
-    @entry.build_picture
+    raise @entry.picture.inspect
      if @entry.save
        redirect_to entries_path, notice: "投稿しました。"
      else

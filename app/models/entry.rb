5b5afd34d786e0c39d3a11a97be99f1fbee1f753
takahiro_u <youdoumei2@gmail.com>
2017-02-05 15:13:29 +0900

    イメージ

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
 #  has_one :picture, class_name: "Picture", dependent: :destroy
-  has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true
+  has_one :image, dependent: :destroy
+  accepts_nested_attributes_for :image, allow_destroy: true
 
 end

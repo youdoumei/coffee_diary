7cf416306a27fe96ffa2a7128364a64a6cbfd22d
takahiro_u <youdoumei2@gmail.com>
2017-02-25 17:40:59 +0900

    投稿の編集画面_更新できるところまで

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,17 @@
 class EntriesController < ApplicationController
   #投稿一覧
   def index
-   @entries = Entry.order("entrynumber")
+    @entries = Entry.order("entrynumber")
   end
 
-  def search
-  end
-
+  #投稿の詳細、編集
   def show
     @entry = Entry.find(params[:id])
-    if params[:format].in?(["jpg", "png", "gif"])
-      send_image #picture
-    else
-      render "show"
-    end
+      if params[:format].in?(["jpg", "png", "gif"])
+        send_image #picture
+       else
+        render "show"
+      end
   end
 
   #新規投稿
@@ -24,7 +22,7 @@ class EntriesController < ApplicationController
 
   def edit
     @entry = Entry.find(params[:id])
-    @entry.build_picture unless @entry.imgae
+    #@entry.build_picture unless @entry.imgae
   end
 
   def create
@@ -37,20 +35,34 @@ class EntriesController < ApplicationController
   end
 
   def update
+    @entry = Entry.find(params[:id])
+    @entry.assign_attributes(entry_params)
+    if @entry.save
+      redirect_to entries_path, notice: "編集しました。"
+    else
+      render "edit"
+    end
+#    @entry.assign_attributes(params[:member])
+#    if @entry.save
+#      redirect_to @member, notice: "編集しました。"
+#    else
+#      render "edit"
+#    end
   end
 
   def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to :entries, notice: "投稿を削除しました"
   end
 
   private
-  def entry_params
-    atts = [:entrynumber, :price, :comment, :entrydate, :value1, :value2, :value3, :value4]
-    atts << { image_attributes: [:_destroy, :id, :photo] }
-    params.require(:entry).permit(atts)
-  #picture_attributes
+    def entry_params
+      atts = [:entrynumber, :price, :comment, :entrydate, :value1, :value2, :value3, :value4]
+      atts << { image_attributes: [:_destroy, :id, :photo] }
+      params.require(:entry).permit(atts)
+    #picture_attributes
+    end
   end
 #20170223--植山
 #エントリーテーブルに列追加するときは、entry_paramsのカラム名を変更する必要がある。
-
-
-#end
